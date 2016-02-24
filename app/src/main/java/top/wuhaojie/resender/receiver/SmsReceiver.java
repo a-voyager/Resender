487159cf5879cb311a732e6a234b74f6a2f71c4c
wuhaojie <w19961009@126.com>
2016-02-24 11:54:58 +0800

    almost complete sms receiver.

--- app/src/main/java/top/wuhaojie/resender/receiver/SmsReceiver.java
+++ app/src/main/java/top/wuhaojie/resender/receiver/SmsReceiver.java
@@ -3,8 +3,15 @@ package top.wuhaojie.resender.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SmsReceiver extends BroadcastReceiver {
+
+    public static final String TAG = "SmsReceiver";
+
     public SmsReceiver() {
     }
 
@@ -12,6 +19,22 @@ public class SmsReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // 如果收到短信
         if ("android.provider.Telephony.SMS_RECEIVED".equals(intent.getAction())) {
+            abortBroadcast();
+            Bundle extras = intent.getExtras();
+            Object[] pdus = (Object[]) extras.get("pdus");
+            SmsMessage[] smsMessages = new SmsMessage[pdus.length];
+            for (int i = 0; i < pdus.length; i++) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], "3gpp");
+                } else {
+                    smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+            }
+            for (SmsMessage message : smsMessages) {
+                String address = message.getDisplayOriginatingAddress();
+                String body = message.getDisplayMessageBody();
+                Log.i(TAG, address + " : " + body);
+            }
         }
     }
 }
